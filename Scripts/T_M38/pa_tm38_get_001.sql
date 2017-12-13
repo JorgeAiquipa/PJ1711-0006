a184c7f43ab38f040065c3e406327460e5abb0d4
cesaractec <cesar.freitas@actecperu.com>
2017-12-13 18:25:17 -0500

    Actualizacion 13.12.2017 por cesar.freitas@actecperu.com

---
+++ Scripts/T_M38/pa_tm38_get_001.sql
@@ -0,0 +1,46 @@
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[pa_tm38_get_001]') AND type in (N'P', N'PC')) --#1
+Drop procedure [dbo].pa_tm38_get_001;
+GO
+
+-- =============================================
+-- Author:		cesar.freitas
+-- Create date: 2017.12.13
+-- Descripcion : Obtiene una lista de los servicios registrados de acuerdo
+-- a una condicion establecida.
+-- =============================================
+CREATE PROCEDURE pa_tm38_get_001
+	@p_TM38_TM2_ID varchar(50),
+	@p_filtro varchar(50)
+AS
+
+BEGIN TRY
+	
+	SET NOCOUNT ON;
+
+
+		SELECT 
+			 TM38_ID
+			,TM38_TM2_ID
+			,TM38_DESCRIP
+			--,TM38_UCREA
+			--,TM38_FCREA
+			--,TM38_UACTUALIZA
+			--,TM38_FACTUALIZA
+
+		FROM
+
+		T_M38
+
+		WHERE 
+		TM38_TM2_ID like @p_TM38_TM2_ID  and TM38_DESCRIP like concat(@p_filtro,'%')
+
+END TRY
+BEGIN CATCH
+	ROLLBACK TRAN
+	
+END CATCH
+GO
+
+--   *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *
+
+EXEC pa_tm38_get_001 'pis' , 's'
---
+++ Win28etug/ET_M38.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Win28etug
+{
+    public class ET_M38
+    {
+        public string _TM38_ID { get; set; }
+        public string _TM38_TM2_ID { get; set; }
+        public string _TM38_DESCRIP { get; set; }
+        public string _TM38_UCREA { get; set; }
+        public DateTime _TM38_FCREA { get; set; }
+        public string _TM38_UACTUALIZA { get; set; }
+        public DateTime _TM38_FACTUALIZA { get; set; }
+        public string _filtro { get; set; }
+    }
+}
